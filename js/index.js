3bb6f5b41c93fdabd8425c04ae548e569566eb4b
Svyatoslavpech <svyatoslavpech@gmail.com>
2022-03-22 23:47:24 -0600

    #9

--- js/index.js
+++ js/index.js
@@ -1,17 +1,25 @@
-//TASK 4
 const submitButton = document.getElementById('submit-button');
 const newTaskNameInput = document.querySelector('#task-name');
 const newAssignedToInput = document.querySelector('#assigned-person');
 const dateInput = document.querySelector('#due-date');
 const newDescriptionInput = document.querySelector('#task-description');
+
 const myTask = new TaskManager;
+
+myTask.load();
+myTask.render();
+
+  let taskName = newTaskNameInput.value;
+  let person = newAssignedToInput.value;
+  let date = dateInput.value;
+  let description = newDescriptionInput.value;
 
 const validFormFieldInput = () => {
   
-   const name = newTaskNameInput.value;
-   const person = newAssignedToInput.value;
-   const date = dateInput.value;
-   const description = newDescriptionInput.value;
+   taskName = newTaskNameInput.value;
+   person = newAssignedToInput.value;
+   date = dateInput.value;
+   description = newDescriptionInput.value;
 
    var myAlert = document.querySelector('.liveAlertPlaceholder');
    myAlert.innerHTML = '';
@@ -20,21 +28,47 @@ const validFormFieldInput = () => {
      if (inputValue.length === 0) {
        myAlert.innerHTML = '<div class="alert alert-warning alert-dismissible" role="alert">' + 'Please input ' 
        + inputName + '<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button></div>';      
-       alertPlaceholder.append(myAlert);
        } 
    }
   
-   warning(name, 'task name');
+   warning(taskName, 'task name');
    warning(person, "a person's name");
    warning(date, 'a date');
    warning(description, 'a task description');
 
-  myTask.addTask(name, description, person, date, 'TODO');
-
 }
 
-//const test = new TaskManager;
-//test.addTask('aaa', 'fff', 'gggg', '01/01/2022');
+submitButton.addEventListener('click', function () {
+  validFormFieldInput();
+  
+  if (taskName && person && date && description) {
+    myTask.addTask(taskName, date, person, description);
+    myTask.render();
+    const taskForm = document.getElementById('task-form');
+    taskForm.reset();
+    myTask.save();
+    
+  }
+} );
 
-submitButton.addEventListener('click', validFormFieldInput);
+const newTasksList = document.querySelector('#tasks-list');
+newTasksList.addEventListener('click', (event) => { 
+  
+    if (event.target.classList.contains("done-button"))  {
+      const parentTask = event.target.parentElement;
+      const taskId = Number(parentTask.dataset.taskId);
+      const task = myTask.getTaskById(taskId);
+      task.status = 'DONE';
+      myTask.render();
+      myTask.save();
+    }
 
+    if (event.target.classList.contains("delete-button")) {
+      const parentTask = event.target.parentElement;
+      const taskId = Number(parentTask.dataset.taskId);
+      myTask.deleteTask(taskId);
+      myTask.render();
+      myTask.save();
+    }
+  
+});
--- js/taskManager.js
+++ js/taskManager.js
@@ -1,18 +1,111 @@
+const createTaskHtml = (name, dueDate, assignedTo, description, status = 'To Do', id) => {
+ 
+  const html = `
+  <li class="list-group-item col-5">
+            <div data-task-id="${id}" class="card">
+              <div class="card-body">
+                <div class="row">
+                  <h5 class="col card-title">${name}</h5>
+                  <h6 class="col card-subtitle text-danger text-end">Due Date: ${dueDate}</h6>
+                </div>
+                <h6 class="card-subtitle mb-2 text-muted">Assigned to: ${assignedTo}</h6>
+                <h6 class="card-subtitle mb-2 text-muted">Status: ${status}</h6>
+                <p class="card-text">${description}</p>
+                <!-- <div class="dropdown d-flex justify-content-end">
+                  <button class="btn btn-light dropdown-toggle" type="button" id="progress" data-bs-toggle="dropdown"
+                    aria-expanded="false">
+                    ${status}
+                  </button>
+                  <ul class="dropdown-menu" aria-labelledby="progress">
+                    <li><a class="dropdown-item">To Do</a></li>
+                    <li><a class="dropdown-item">In Progress</a></li>
+                    <li><a class="dropdown-item">Review</a></li>
+                    <li><a class="dropdown-item">Done</a></li>
+                  </ul>
+                </div>-->
+              </div>
+              <button type="button" id="tasks-list" class="btn btn-success done-button">Mark As Done</button>
+              <button type="button" id="tasks-list" class="btn btn-danger delete-button">Delete</button>
+            </div>
+          </li>
+  `;
+  return html;
+}
+
 class TaskManager {
   constructor(currentId = 0) {
     this.currentId = currentId;
     this.tasks = [];
   }
-  addTask = (name, description, assignedTo, dueDate, status = 'TODO') => {
-    const taskObj = [
-    this.currentId ++,
-    name,
-    description,
-    assignedTo,
-    dueDate,
-    'TODO'
-  ];
-    this.tasks.push(taskObj);
-    console.log(this.tasks);
+
+  addTask (name, dueDate, assignedTo, description, status = 'TO DO') {
+      this.currentId++;
+    
+    this.tasks.push( {currentId: this.currentId, name: name, dueDate: dueDate, assignedTo: assignedTo, description: description, status: status} );
+    
   }
+
+  render() {
+    const tasksHtmlList = [];
+    
+    for (let i = 0; i < this.tasks.length; i++) {
+      //store current task in a variable
+      const currentTask = this.tasks[i];
+      
+      const date = new Date(currentTask.dueDate);
+      const formattedDate = (date.getMonth() + 1) + '/' + (date.getDate() + 1) + '/' + date.getFullYear();
+      //adding 1 to getMonth() because array of months begins with 0=January...
+      const taskHtml = createTaskHtml(currentTask.name, formattedDate, currentTask.assignedTo, currentTask.description, currentTask.status, currentTask.currentId);
+      
+      tasksHtmlList.push(taskHtml)
+    }
+
+    const tasksHtml = tasksHtmlList.join('\n');
+    const tasksList = document.getElementById('tasks-list');
+    tasksList.innerHTML = tasksHtml;
+  }
+
+  getTaskById(taskId) {
+    
+    let foundTask;
+    for (let i = 0; i < this.tasks.length; i++) {
+      let task = this.tasks[i];
+      if (taskId === task.currentId) {
+        foundTask = task;
+      }
+    }
+    return foundTask;
+  }
+
+  save() {
+    const tasksJson = JSON.stringify(this.tasks);
+    localStorage.setItem('tasks', tasksJson);
+    const currentId = JSON.stringify(this.currentId);
+    localStorage.setItem('currentId', currentId);
+  }
+
+  load() {
+    if (localStorage.getItem('tasks')) {
+      const tasksJson = localStorage.getItem('tasks');
+      console.log(JSON.parse(tasksJson));
+      this.tasks = (JSON.parse(tasksJson));
+    }
+    if (localStorage.getItem('currentId')) {
+      const currentId = localStorage.getItem('currentId');
+      this.currentId = Number(currentId)
+    }
+  }
+
+  deleteTask(taskId)  {
+    var newTasks = [];
+    for (let i = 0; i < this.tasks.length; i++) {
+      const task = this.tasks[i];
+      if (this.tasks[i].currentId != taskId) {
+        newTasks.push(task);
+        console.log("newTasks=", newTasks);
+      }
+    }
+    this.tasks = newTasks;
+  }
+
 }
